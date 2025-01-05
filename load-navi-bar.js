45f012ac25705ae1b409f2ba635a1bbe9e3347a0
himanshu111203 <rohillahimanshu42@gmail.com>
2025-01-05 23:14:46 +0530

    Create load-navi-bar.js

---
+++ load-navi-bar.js
@@ -0,0 +1,14 @@
+function loadNavigation() {
+    const navDiv = document.getElementById('navigation');
+    fetch('navigation.html')
+        .then(response => response.text())
+        .then(data => {
+            navDiv.innerHTML = data;
+
+            highlightActiveLink();
+        })
+        .catch(error => console.error('Error loading navigation:', error));
+}
+
+
+loadNavigation();
